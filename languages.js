6186554c7fe4536532bd9b0e71a83354ca5d73b6
faith <143903684+Faith-Kpogo@users.noreply.github.com>
2024-03-04 13:35:21 +0100

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,13 @@
+const languages = [
+  {
+    no: "0",
+    name: "English",
+    native: "English",
+    code: "en",
+  },{
+    no: "1",
+    name: "Twi",
+    native: "Twi",
+    code: "tw",
+  },  
+];
